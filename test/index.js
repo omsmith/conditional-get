0d9c73235a17062782bc108254e5a8b7fa0ebfb1
Owen Smith <owen@omsmith.ca>
2015-10-27 23:03:26 -0400

    src,test: update to (ctx, next) -> Promise middleware contract

--- test/index.js
+++ test/index.js
@@ -2,49 +2,49 @@
 var request = require('supertest');
 var conditional = require('..');
 var etag = require('koa-etag');
-var koa = require('koa');
+var Koa = require('koa');
 var fs = require('fs');
 
 describe('conditional()', function(){
   describe('when cache is fresh', function(){
     it('should respond with 304', function(done){
-      var app = koa();
+      var app = new Koa();
 
       app.use(conditional());
       app.use(etag());
 
-      app.use(function *(next){
-        yield next;
-
-        this.body = {
-          name: 'tobi',
-          species: 'ferret',
-          age: 2
-        };
+      app.use(function (ctx, next){
+        return next().then(function(){
+          ctx.body = {
+            name: 'tobi',
+            species: 'ferret',
+            age: 2
+          };
+        });
       })
 
       request(app.listen())
       .get('/')
-      .set('If-None-Match', '"-1572211827"')
+      .set('If-None-Match', '"2a-saXVat/5URRlHnxWdwdDeg"')
       .expect(304, done);
     })
   })
 
   describe('when cache is stale', function(){
     it('should do nothing', function(done){
-      var app = koa();
+      var app = new Koa();
 
       app.use(conditional());
       app.use(etag());
 
-      app.use(function *(next){
-        yield next;
-
-        this.body = {
-          name: 'tobi',
-          species: 'ferret',
-          age: 2
-        };
+      app.use(function (ctx, next){
+        return next().then(function(){
+          ctx.body = {
+            name: 'tobi',
+            species: 'ferret',
+            age: 2
+          };
+        });
       })
 
       request(app.listen())
